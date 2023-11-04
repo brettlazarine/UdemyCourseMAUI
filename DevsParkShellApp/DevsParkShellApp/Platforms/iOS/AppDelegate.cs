811aa022db13ab9a1780d2d1df030ad58dd6249d
Brett Lazarine <jblazarine@gmail.com>
2023-11-03 22:48:06 -0500

    create DevsPark app

---
+++ DevsParkShellApp/DevsParkShellApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DevsParkShellApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
